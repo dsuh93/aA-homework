6d119e2f97d984a362a915e601f3047dd77dee3f
David Suh <davidisuh93@gmail.com>
2020-12-16 02:17:23 -0500

    first js hw

---
+++ w8d3/problem_sets.js
@@ -0,0 +1,15 @@
+// madLib
+// Write a function that takes three strings
+// - a verb, an adjective, and a noun
+// - uppercases and interpolates them into the sentence
+// "We shall VERB the ADJECTIVE NOUN". Use ES6 template literals.
+
+// For example,
+
+// > madLib('make', 'best', 'guac');
+// "We shall MAKE the BEST GUAC."
+
+function madLib(verb, adj, noun) {
+    console.log(`We shall ${verb.prototype.toUpperCase} the ${adj.prototype.toUpperCase}
+    ${noun.prototype.toUpperCase}.`)
+}
